34435db480edfe6ac3be0dd6657d1ef0d09521ea
Link <h26281660@gmail.com>
2024-04-26 20:56:21 +0800

    add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+  en: 'English',
+  'zh-TW': '中文',
+};
+
+export const defaultLang = 'zh-TW';
+
+export const ui = {
+  en: {
+    name: 'I’m LIN, YI-LUN',
+    web_developer: 'Web Developer',
+    self_intro_1:
+      'I am a passionate web developer who cares about code quality and best practices, and I am always eager to learn and explore new technologies to stay competitive in the ever-evolving tech landscape. My areas of expertise include frontend and backend technologies.',
+    self_intro_2:
+      'I have extensive experience in frontend development, leveraging Vue.js, Nuxt.js, Astro, and TypeScript to build modern, performant, and user-friendly web applications.',
+    self_intro_3:
+      "For backend development, I am proficient in using Node.js's Express and PHP's Laravel to build robust and efficient backend systems. I am also passionate about developing scalable applications to meet ever-changing needs.",
+  },
+  'zh-TW': {
+    name: 'I’m 林意倫 LIN, YI-LUN',
+    web_developer: '網站開發工程師 Web Developer',
+    self_intro_1:
+      '我是一位熱愛網站開發的工程師，注重代碼品質和最佳實踐，並且樂於學習和探索新的技術，以保持在不斷演進的科技環境中的競爭力，專業領域主要包括前端和後端技術。',
+    self_intro_2:
+      '在前端方面具我有豐富的經驗，擅長使用 Vue.js、Nuxt.js、Astro 和 Typescript 進行開發。這些工具和框架讓我能夠建立現代化、高效且具有優良使用者體驗的網頁應用程式。',
+    self_intro_3:
+      '在後端方面，我使用 Nodejs 的 Express 和 PHP 的 Laravel，能夠構建穩健且高效的後端系統，並致力於開發可擴展的應用程式，以滿足不斷變化的需求。',
+  },
+} as const;
